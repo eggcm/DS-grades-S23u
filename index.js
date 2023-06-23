0891495d85437f3cf28dcea9a856742fcd3378b2
boboxin <66729964+boboxin@users.noreply.github.com>
2023-06-23 17:52:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ let data = {}
 const p = document.getElementById('display-grade');
 
 
-Papa.parse("DS-grades-S23-EECS.csv", {
+// Papa.parse("DS-grades-S23-EECS.csv", {
+Papa.parse("test.csv", {
 	download: true,
     encoding: "utf-8",
     complete: function(results) {
@@ -29,16 +30,26 @@ function display() {
     // p.innerText = JSON.stringify(data[id]);
     const display = `
     SID:${data[id].學號}
+    
+    各項作業分數：
     H1: ${data[id].H1}
     H2: ${data[id].H2}
     H3: ${data[id].H3}
     H4: ${data[id].H4}
     H5: ${data[id].H5}
-    H5: ${data[id].H6}
-    Mid: ${data[id].Mid}
-    Fin: ${data[id].Fin}
+    H6: ${data[id].H6}
+
+    考試分數：
+    Mid:${data[id].Mid}
+    Fin:${data[id].Fin}
+    
+    CPE加分項目：
     CPE: ${data[id].CPE}
+    
+    學期總成績：
     Sem: ${data[id].Sem}
+    
+    分數計算方式：
     The grade is determined by the following rule: (H1+H2+H3+H4+H5+H6)/6*0.4+Mid*0.25+FIN*0.35+CPE.
     Note that the highest grade of this course is 99. If your final grade is large than 99, then you will get 99 at most.
     `;
